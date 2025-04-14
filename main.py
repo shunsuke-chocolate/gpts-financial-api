23d303b0656157b732ea1fc5d5e79c3f0735d5de
shunsuke-chocolate <shuy1.nsk921@gmail.com>
2025-04-14 23:30:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List, Dict, Optional
+from typing import Dict, List
 from financial_graph_generators import (
     plot_bs_vertical_grouped_stacked,
     plot_profit_step_only,
-    plot_cf_waterfall,
+    plot_cf_waterfall_labeled
 )
 
 app = FastAPI()
 
+
 class BSInput(BaseModel):
-    bs_data: Dict[str, Dict[str, List[float]]]
-    font_path: Optional[str] = "./NotoSerifJP-Regular.ttf"
+    bs_data: Dict[str, Dict[str, float]]
+    font_path: str
     years: List[str]
     title: str
 
+
 class ProfitStepInput(BaseModel):
-    df_middle: Dict[str, Dict[str, float]]
-    font_path: Optional[str] = "./NotoSerifJP-Regular.ttf"
+    df_middle: Dict[str, float]
+    font_path: str
+
 
 class CFItem(BaseModel):
     label: str
     amount: float
 
+
 class CFInput(BaseModel):
     cf_values: List[CFItem]
-    font_path: Optional[str] = "./NotoSerifJP-Regular.ttf"
+    font_path: str
     title: str
+
 
 @app.post("/plot_bs")
 def plot_bs(input_data: BSInput):
     output_path = plot_bs_vertical_grouped_stacked(
         input_data.bs_data,
-        input_data.years,
         input_data.font_path,
+        input_data.years,
         input_data.title
     )
     return {"path": output_path}
+
 
 @app.post("/plot_profit_step")
 def plot_profit_step(input_data: ProfitStepInput):
@@ -46,10 +52,11 @@ def plot_profit_step(input_data: ProfitStepInput):
     )
     return {"path": output_path}
 
+
 @app.post("/plot_cf")
 def plot_cf(input_data: CFInput):
     cf_data = [item.dict() for item in input_data.cf_values]
-    output_path = plot_cf_waterfall(
+    output_path = plot_cf_waterfall_labeled(
         cf_data,
         input_data.font_path,
         input_data.title
